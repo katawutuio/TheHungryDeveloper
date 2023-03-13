8b52309e05d959910054a19771ccbae326148483
Katawut Keng <katawutu.io@gmail.com>
2023-03-13 23:21:32 +0700

    Setup intent to new Activity

--- app/src/main/java/com/proudsoft/thehungrydeveloper/MainActivity.java
+++ app/src/main/java/com/proudsoft/thehungrydeveloper/MainActivity.java
@@ -3,7 +3,9 @@ package com.proudsoft.thehungrydeveloper;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     CardView cardViewStarters;
@@ -16,5 +18,13 @@ public class MainActivity extends AppCompatActivity {
 
         cardViewStarters = findViewById(R.id.card_view_starters);
         cardViewMains = findViewById(R.id.card_view_mains);
+
+        cardViewStarters.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startersActivityIntent = new Intent(MainActivity.this, StartersActivity.class);
+                startActivity(startersActivityIntent);
+            }
+        });
     }
 }
