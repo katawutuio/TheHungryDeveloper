0bca4dc13640debd29168e102e20035e7722dcd5
Katawut Keng <katawutu.io@gmail.com>
2023-03-13 22:42:28 +0700

    update code

--- app/src/main/java/com/proudsoft/thehungrydeveloper/MainActivity.java
+++ app/src/main/java/com/proudsoft/thehungrydeveloper/MainActivity.java
@@ -1,14 +1,20 @@
 package com.proudsoft.thehungrydeveloper;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    CardView cardViewStarters;
+    CardView cardViewMains;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cardViewStarters = findViewById(R.id.card_view_starters);
+        cardViewMains = findViewById(R.id.card_view_mains);
     }
 }
